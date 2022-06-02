21a77367d9c2c8018a091cfa97abbd1df917af38
Ian Codner <90807772+Ian-Codner@users.noreply.github.com>
2022-06-02 14:14:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,50 @@
-let DIRT = 0;
+function setup() {
+  createCanvas(800, 800);
+  background(255);
+
+  int x = 0;
+  while (x < 800) {
+    randomRectangle();
+    x++;
+  }
+}
+
+function randomRectangle() {
+  //for random colors and sizes
+  fill(random(256), random(256), random(256));
+  rect(random(800), random(800), random(150), random(150));
+  
+//remnants of the code of my failed attempt to det the set color rectangles
+//  while ( = 0) {
+//    fill(255);
+//    rect(random(800), random(800), random(150), random(150));
+//  }
+//  while (y = 1) {
+//    fill(0);
+//    rect(random(800), random(800), random(150), random(150));
+//  }
+//  while (y = 2) {
+//   fill(255, 0, 0);
+//    rect(random(800), random(800), random(150), random(150));
+//  }
+//  while (y = 3) {
+//    fill(0, 255, 0);
+//    rect(random(800), random(800), random(150), random(150));
+//  }
+//  while (y = 4) {
+//    fill(0, 0, 255);
+//    rect(random(800), random(800), random(150), random(150));
+//  }
+}
+
+
+
+
+
+
+
+
+/*let DIRT = 0;
 let FIRE = 1;
 let BURNT = 2;
 let TREE = 3;
@@ -98,3 +144,4 @@ class Land {
   }
   
 }
+*/
